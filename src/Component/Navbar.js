7d7ec6da06990eca5f0dfecdabd95cff1f9e4f25
Rupasharma18 <rupa18@navgurukul.org>
2020-07-03 09:57:52 +0530

    navbar color changed

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -5,7 +5,7 @@ import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+// import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 const useStyles = makeStyles((theme) => ({
